f78cc4216e2c8d146f12df2024565a0c970faa6c
MoonStar <moonsung0331@gmail.com>
2023-07-26 17:36:56 +0900

    feat: RESTful 한 응답을 하기 위한 Response 코드 구현

---
+++ src/main/java/com/genai/tcoop/response/Response.java
@@ -0,0 +1,28 @@
+package com.genai.tcoop.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response<T> implements Serializable {
+
+    private String resultCode;
+    private T result;
+
+    public static Response<Void> error(String errorCode) {
+        return new Response<>(errorCode, null);
+    }
+
+    public static Response<Void> success() {
+        return new Response<Void>("SUCCESS", null);
+    }
+
+    public static <T> Response<T> success(T result) {
+        return new Response<>("SUCCESS", result);
+    }
+}
