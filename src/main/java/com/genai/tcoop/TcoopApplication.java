941e5adb6c0adfb945990b2bfde063616ef0fca4
MoonStar <moonsung0331@gmail.com>
2023-07-26 16:48:34 +0900

    Project Init

---
+++ src/main/java/com/genai/tcoop/TcoopApplication.java
@@ -0,0 +1,13 @@
+package com.genai.tcoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcoopApplication.class, args);
+    }
+
+}
