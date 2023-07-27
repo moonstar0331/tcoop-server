0cd040ecc98ad80387838efd622cd346066433a6
MoonStar <moonsung0331@gmail.com>
2023-07-27 16:03:10 +0900

    feat: Planner 수정 기능 구현

--- src/main/java/com/genai/tcoop/controller/PlannerController.java
+++ src/main/java/com/genai/tcoop/controller/PlannerController.java
@@ -2,6 +2,7 @@ package com.genai.tcoop.controller;
 
 import com.genai.tcoop.model.dto.PlannerDTO;
 import com.genai.tcoop.model.dto.request.PlannerCreateRequest;
+import com.genai.tcoop.model.dto.request.PlannerUpdateRequest;
 import com.genai.tcoop.model.dto.response.PlannerResponse;
 import com.genai.tcoop.model.dto.response.Response;
 import com.genai.tcoop.service.PlannerService;
@@ -19,6 +20,12 @@ public class PlannerController {
     @PostMapping
     public Response<PlannerResponse> create(@RequestBody PlannerCreateRequest request, Authentication authentication) {
         PlannerDTO planner = plannerService.create(request.getTitle(), authentication.getName());
+        return Response.success(PlannerResponse.fromPlannerDto(planner));
+    }
+
+    @PatchMapping("/{plannerId}")
+    public Response<PlannerResponse> update(@PathVariable Long plannerId, @RequestBody PlannerUpdateRequest request, Authentication authentication) {
+        PlannerDTO planner = plannerService.update(plannerId, request.getTitle(), authentication.getName());
         return Response.success(PlannerResponse.fromPlannerDto(planner));
     }
 }
--- src/main/java/com/genai/tcoop/service/PlannerService.java
+++ src/main/java/com/genai/tcoop/service/PlannerService.java
@@ -20,7 +20,7 @@ public class PlannerService {
 
     @Transactional
     public PlannerDTO create(String title, String userAccountId) {
-        // 사용자 validation
+        // 사용자 존재 여부 validation
         UserAccount user = userAccountRepository.findByUserAccountId(userAccountId)
                 .orElseThrow(() -> new TcoopException(ErrorCode.USER_NOT_FOUND, String.format("%s is not found", userAccountId)));
 
@@ -32,5 +32,28 @@ public class PlannerService {
 
         // entity -> dto
         return PlannerDTO.fromEntity(saved);
+    }
+
+    @Transactional
+    public PlannerDTO update(Long plannerId, String title, String userAccountId) {
+        // 사용자 존재 여부 validation
+        UserAccount user = userAccountRepository.findByUserAccountId(userAccountId)
+                .orElseThrow(() -> new TcoopException(ErrorCode.USER_NOT_FOUND, String.format("%s is not found", userAccountId)));
+
+        // planner 존재 여부 validation
+        Planner planner = plannerRepository.findById(plannerId)
+                .orElseThrow(() -> new TcoopException(ErrorCode.PLANNER_NOT_FOUND));
+
+        // planner 작성자와 로그인 사용자 validation
+        if(planner.getUser() != user) {
+            throw new TcoopException(ErrorCode.INVALID_PERMISSION,
+                    String.format("%s has no permission with %s", userAccountId, plannerId));
+        }
+
+        // planner 수정
+        planner.updateTitle(title);
+        Planner updated = plannerRepository.save(planner);
+
+        return PlannerDTO.fromEntity(updated);
     }
 }
