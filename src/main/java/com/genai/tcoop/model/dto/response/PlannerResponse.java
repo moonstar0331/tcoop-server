7fab890a49109df83f90890f6344a1251c9f54e0
MoonStar <moonsung0331@gmail.com>
2023-07-27 16:06:16 +0900

    feat: PlannerResponse 속성 추가
    Planner 의 생성일자와 수정일자를 알 수 있도록 수정함

--- src/main/java/com/genai/tcoop/model/dto/response/PlannerResponse.java
+++ src/main/java/com/genai/tcoop/model/dto/response/PlannerResponse.java
@@ -4,6 +4,8 @@ import com.genai.tcoop.model.dto.PlannerDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class PlannerResponse {
@@ -11,8 +13,11 @@ public class PlannerResponse {
     private Long plannerId;
     private String title;
     private String userAccountId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public static PlannerResponse fromPlannerDto(PlannerDTO dto) {
-        return new PlannerResponse(dto.getId(), dto.getTitle(), dto.getUser().getUsername());
+        return new PlannerResponse(dto.getId(), dto.getTitle(),
+                dto.getUser().getUsername(), dto.getCreatedAt(), dto.getUpdatedAt());
     }
 }
