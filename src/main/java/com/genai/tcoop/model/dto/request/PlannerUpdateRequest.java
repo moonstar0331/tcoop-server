6814d68339ca5adc8101c27edaa9c519c675b2f1
MoonStar <moonsung0331@gmail.com>
2023-07-27 16:02:59 +0900

    feat: Planner 수정을 위한 Request 작성

---
+++ src/main/java/com/genai/tcoop/model/dto/request/PlannerUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.genai.tcoop.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlannerUpdateRequest {
+    private String title;
+}
