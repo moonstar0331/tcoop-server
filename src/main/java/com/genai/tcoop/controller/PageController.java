97d13b62a6e5411e50ab3dd408b2ddc868163fb9
MoonStar <moonsung0331@gmail.com>
2023-07-31 16:50:16 +0900

    feat: 프로토타입 뷰 컨트롤러 구현

---
+++ src/main/java/com/genai/tcoop/controller/PageController.java
@@ -0,0 +1,34 @@
+package com.genai.tcoop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/login";
+    }
+
+    @GetMapping("/my")
+    public String my() {
+        return "user/my";
+    }
+
+    @GetMapping("/planners/{plannerId}")
+    public String planner(@PathVariable Long plannerId) {
+        return "planner/planner";
+    }
+
+    @GetMapping("/planners/list")
+    public String myPlanners() {
+        return "user/planners";
+    }
+
+    @GetMapping("/planners/create")
+    public String createPlanner() {
+        return "planner/create";
+    }
+}
