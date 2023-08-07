22f5a6819b7b7de30c03a16c83516d16d2e5eab7
MoonStar <moonsung0331@gmail.com>
2023-08-08 02:06:10 +0900

    feat: GPT API 사용만을 위한 Request Response DTO 작성

---
+++ src/main/java/com/genai/tcoop/openai/ChatRequest.java
@@ -0,0 +1,25 @@
+package com.genai.tcoop.openai;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRequest {
+
+    private String model;
+    private List<Message> messages;
+    private int n;
+    private double temperature;
+
+    public ChatRequest(String model, String prompt) {
+        this.model = model;
+        this.messages = new ArrayList<>();
+        this.messages.add(new Message("user", prompt));
+    }
+}
