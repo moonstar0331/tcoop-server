a9392b21e244b5a86917a28bf7687f1f5b25b820
MoonStar <moonsung0331@gmail.com>
2023-08-23 15:03:25 +0900

    필터링된 Keywords와 생성된 Comment 를 리턴을 위한 Response DTO 생성

---
+++ src/main/java/com/genai/tcoop/openai/KeywordsWithCommentResponse.java
@@ -0,0 +1,16 @@
+package com.genai.tcoop.openai;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KeywordsWithCommentResponse {
+
+    private List<String> keywords;
+    private String comment;
+}
