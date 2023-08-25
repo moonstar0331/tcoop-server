3a19ebac5f9d24cb88063e16934b019e66210bce
MoonStar <moonsung0331@gmail.com>
2023-08-25 14:13:46 +0900

    Request method 'POST' not supported 에러 해결을 위한 HttpMethod 추가

--- src/main/java/com/genai/tcoop/openai/GptController.java
+++ src/main/java/com/genai/tcoop/openai/GptController.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api", method = {RequestMethod.GET, RequestMethod.POST})
 @RequiredArgsConstructor
 public class GptController {
 
