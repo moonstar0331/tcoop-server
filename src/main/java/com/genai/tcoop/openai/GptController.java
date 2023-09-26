a08965353a45c37ec88655da0bddb7349bf53f28
MoonStar <moonsung0331@gmail.com>
2023-09-26 10:29:03 +0900

    feat: 관리자에게 직접 받은 prompt를 통해 gpt와 통신하는 로직 구현

--- src/main/java/com/genai/tcoop/openai/GptController.java
+++ src/main/java/com/genai/tcoop/openai/GptController.java
@@ -1,6 +1,12 @@
 package com.genai.tcoop.openai;
 
 import com.genai.tcoop.model.dto.response.Response;
+import com.genai.tcoop.openai.request.KeywordRequest;
+import com.genai.tcoop.openai.request.UserPromptRequest;
+import com.genai.tcoop.openai.response.CommentResponse;
+import com.genai.tcoop.openai.response.KeywordsResponse;
+import com.genai.tcoop.openai.response.KeywordsWithCommentResponse;
+import com.genai.tcoop.openai.response.PromptContentResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -9,18 +15,18 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/gpt")
 @RequiredArgsConstructor
 public class GptController {
 
     private final GptService gptService;
 
-    @PostMapping("/gpt")
+    @PostMapping
     public Response<KeywordsWithCommentResponse> chat(@RequestBody KeywordRequest request) {
         return Response.success(gptService.callGpt(request.getKeywords()));
     }
 
-    @PostMapping("/gpt/filtering")
+    @PostMapping("/filtering")
     public Response<KeywordsResponse> filtering(@RequestBody KeywordRequest request) {
         log.info("before filtering: {}", request.getKeywords());
         List<String> filteredKeywords = gptService.callGptForFiltering(request.getKeywords());
@@ -28,10 +34,16 @@ public class GptController {
         return Response.success(new KeywordsResponse(filteredKeywords));
     }
 
-    @PostMapping("/gpt/comment")
+    @PostMapping("/comment")
     public Response<CommentResponse> comment(@RequestBody KeywordRequest request) {
         String comment = gptService.callGptForCreateComment(request.getKeywords());
         log.info("Created comment: {}", comment);
         return Response.success(new CommentResponse(comment));
+    }
+
+    @PostMapping("/prompt")
+    public Response<PromptContentResponse> filteringWithPrompt(@RequestBody UserPromptRequest request) {
+        String gptContent = gptService.callGptWithPrompt(request.getKeywords(), request.getPrompt());
+        return Response.success(new PromptContentResponse(gptContent));
     }
 }
